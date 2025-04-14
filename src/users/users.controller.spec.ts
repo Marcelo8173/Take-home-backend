33f1553c10a3da7d4552cba1dea6d1fa6733bd82
Marcelo8173 <marceloandrebio@gmail.com>
2025-04-14 18:55:51 -0300

    create a unit test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,80 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UserService } from './users.service';
+import { UserCreateDTO } from './DTOs/UserCreateDTO';
+import { UpdateUserDto } from './DTOs/UpdateUserDto';
+import { User } from './user.entity';
+import { DeleteResult } from 'typeorm';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UserService;
+
+  const mockUser: User = {
+    id: 'uuid-mockado',
+    name: 'Marcelo',
+    email: 'marcelo@example.com',
+  };
+
+  const mockUserService = {
+    createUserService: jest.fn().mockResolvedValue(mockUser),
+    listAllUserService: jest.fn().mockResolvedValue([mockUser]),
+    listAllUserByIdService: jest.fn().mockResolvedValue(mockUser),
+    updateUserService: jest
+      .fn()
+      .mockResolvedValue({ ...mockUser, name: 'Atualizado' }),
+    deleteUserSerivce: jest
+      .fn()
+      .mockResolvedValue({ affected: 1 } as DeleteResult),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('shoul be create a new user', async () => {
+    const dto: UserCreateDTO = {
+      name: 'Marcelo',
+      email: 'marcelo@example.com',
+    };
+    expect(await controller.createUser(dto)).toEqual(mockUser);
+    expect(service.createUserService).toHaveBeenCalledWith(dto);
+  });
+
+  it('should be able lista all user', async () => {
+    expect(await controller.listAllUser()).toEqual([mockUser]);
+  });
+
+  it('shoubl be able return user by id', async () => {
+    expect(await controller.listAllUserById('uuid-mockado')).toEqual(mockUser);
+  });
+
+  it('should be able update user', async () => {
+    const dto: UpdateUserDto = {
+      name: 'Atualizado',
+      email: 'marceloandre@gmail.com',
+    };
+    expect(await controller.updateUserById('uuid-mockado', dto)).toEqual({
+      ...mockUser,
+      name: 'Atualizado',
+    });
+  });
+
+  it('it should be able delete user', async () => {
+    expect(await controller.deleteByUser('uuid-mockado')).toEqual({
+      affected: 1,
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,129 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import { UserService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { DeleteResult } from 'typeorm';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { UserCreateDTO } from './DTOs/UserCreateDTO';
+import { UpdateUserDto } from './DTOs/UpdateUserDto';
 
-describe('UsersService', () => {
-  let service: UsersService;
+describe('UserService', () => {
+  let service: UserService;
+
+  const mockUser: User = {
+    id: 'uuid-mockado',
+    name: 'Marcelo',
+    email: 'marcelo@example.com',
+  };
+
+  const mockRepository = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    findOneBy: jest.fn(),
+    preload: jest.fn(),
+    delete: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    service = module.get<UserService>(UserService);
+    jest.clearAllMocks();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('shoud be create a new user', async () => {
+    mockRepository.findOne.mockResolvedValue(undefined);
+    mockRepository.create.mockReturnValue(mockUser);
+    mockRepository.save.mockResolvedValue(mockUser);
+
+    const dto: UserCreateDTO = {
+      name: 'Marcelo',
+      email: 'marcelo@example.com',
+    };
+
+    const result = await service.createUserService(dto);
+    expect(result).toEqual(mockUser);
+    expect(mockRepository.findOne).toHaveBeenCalledWith({
+      where: { email: dto.email },
+    });
+    expect(mockRepository.create).toHaveBeenCalledWith(dto);
+    expect(mockRepository.save).toHaveBeenCalledWith(mockUser);
+  });
+
+  it('not be able create a new user if exist a user', async () => {
+    mockRepository.findOne.mockResolvedValue(mockUser);
+
+    await expect(
+      service.createUserService({
+        name: 'Marcelo',
+        email: 'marcelo@example.com',
+      }),
+    ).rejects.toThrow(BadRequestException);
+  });
+
+  it('should be able list all user', async () => {
+    mockRepository.find.mockResolvedValue([mockUser]);
+    const result = await service.listAllUserService();
+    expect(result).toEqual([mockUser]);
+  });
+
+  it('shoudl be able list user by id', async () => {
+    mockRepository.findOneBy.mockResolvedValue(mockUser);
+    const result = await service.listAllUserByIdService('uuid-mockado');
+    expect(result).toEqual(mockUser);
+  });
+
+  it('should throw an error if user is not found by id', async () => {
+    mockRepository.findOneBy.mockResolvedValue(null);
+    await expect(
+      service.listAllUserByIdService('uuid-invalido'),
+    ).rejects.toThrow(NotFoundException);
+  });
+
+  it('should update the user if they exist', async () => {
+    const updatedUser = { ...mockUser, name: 'Atualizado' };
+    mockRepository.preload.mockResolvedValue(updatedUser);
+    mockRepository.save.mockResolvedValue(updatedUser);
+
+    const dto: UpdateUserDto = {
+      name: 'Atualizado',
+      email: 'marceloandrebio@gmail.com',
+    };
+
+    const result = await service.updateUserService('uuid-mockado', dto);
+    expect(result).toEqual(updatedUser);
+    expect(mockRepository.preload).toHaveBeenCalledWith({
+      id: 'uuid-mockado',
+      ...dto,
+    });
+    expect(mockRepository.save).toHaveBeenCalledWith(updatedUser);
+  });
+
+  it('should throw an error when trying to update a non-existent user', async () => {
+    mockRepository.preload.mockResolvedValue(null);
+    const dto: UpdateUserDto = {
+      name: 'Inexistente',
+      email: 'marceloandrebio@gmail.com',
+    };
+    await expect(service.updateUserService('uuid-fake', dto)).rejects.toThrow(
+      NotFoundException,
+    );
+  });
+
+  it('should delete a user', async () => {
+    const mockDeleteResult: DeleteResult = { affected: 1, raw: [] };
+    mockRepository.delete.mockResolvedValue(mockDeleteResult);
+    const result = await service.deleteUserSerivce('uuid-mockado');
+    expect(result).toEqual(mockDeleteResult);
   });
 });
