14b071701afe069e651aa658757bf88a1fcfa12a
Marcelo8173 <marceloandrebio@gmail.com>
2025-04-14 22:20:42 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+    allowedHeaders: '*',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
